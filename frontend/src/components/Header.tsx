3c21714f2b67b9f864589acb24f12637accb2549
Lisa Sundgren <lisasundgren@hotmail.com>
2026-08-10 12:50:35 +0200

    Updated header design and database changes

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -3,17 +3,20 @@ import { Link } from 'react-router-dom';
 
 const Header = (): ReactElement => {
   return (
-    <header className="bg-light p-3 border-bottom">
-      <nav className="container d-flex justify-content-between align-items-center">
-        <Link to="/" className="text-decoration-none text-dark fs-2 d-flex align-items-center">
-          <i className="bi bi-fork-knife text-success"></i>
-          <span className="fw-bold"> Mealplanner</span>
+    <header className="bg-light px-3 py-2 border-bottom">
+      <nav className="container px-0 d-flex justify-content-between align-items-center gap-2">
+        <Link
+          to="/"
+          className="text-decoration-none text-dark fs-4 fs-md-2 d-flex align-items-center py-2"
+        >
+          <i className="bi bi-fork-knife text-success me-1"></i>
+          <span className="fw-bold">Mealplanner</span>
         </Link>
-        <div className="d-flex gap-3">
-          <Link to="/" className="text-decoration-none text-secondary">
+        <div className="d-flex align-items-center gap-3 text-nowrap">
+          <Link to="/" className="text-decoration-none text-secondary py-2">
             Home
           </Link>
-          <Link to="grocerylist" className="text-decoration-none text-secondary">
+          <Link to="grocerylist" className="text-decoration-none text-secondary py-2">
             Grocery List
           </Link>
         </div>
